72c4bdfdde05d864d955fd6fc681b1a28d5120db
Gerald Lopez <gerald.lopez@alumnos.ucn.cl>
2020-09-16 10:29:20 -0300

    add docs

---
+++ docs/precache-manifest.76c219e3edf5c8cc41ef49c378ed3244.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ce3573c080ecfb4b63198318dc64be2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb27eb60310dfdd5bbbe",
+    "url": "/static/css/main.cc2d3e84.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb27eb60310dfdd5bbbe",
+    "url": "/static/js/main.c1312a6f.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
